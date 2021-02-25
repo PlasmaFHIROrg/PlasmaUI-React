f3bf50a8af4bd4bb628303e7b944de67fb106714
PF <blank@email.com>
2021-02-25 15:02:45 -0600

    familymemberhistory view

---
+++ src/components/FamilyMemberHistoryView.tsx
@@ -0,0 +1,51 @@
+import React from 'react';
+import FamilyMemberHistory from "./../models/FamilyMemberHistory";
+import './FamilyMemberHistoryView.css';
+
+import FamilyMemberHistory_Condition from "./../models/FamilyMemberHistory_Condition";
+import CodeableConcept from "./../models/CodeableConcept";
+import { CodeableConceptView } from "./CodeableConceptView";
+
+export interface IFamilyMemberHistoryViewProps { familyMemberHistory?: FamilyMemberHistory; }
+export const FamilyMemberHistoryView: React.FC<IFamilyMemberHistoryViewProps> = (props) => {
+  if (!props.familyMemberHistory) { return <div />; }
+  const { familyMemberHistory } = props;
+
+  // Relationship...
+  let elRelationship = null;
+  if (familyMemberHistory.relationship) {
+    elRelationship = <CodeableConceptView codeableConcept={familyMemberHistory.relationship} />
+  }
+
+  // Sex...
+  let elSex = null;
+  if (familyMemberHistory.sex) {
+    elSex = <CodeableConceptView codeableConcept={familyMemberHistory.sex}  />
+  }
+
+  // TODO: Make a FamilyMemberHistory_ConditionView
+
+  // Conditions...
+  let elConditions = null;
+  if (familyMemberHistory.condition) {
+    // Convert to array...
+    let condition = familyMemberHistory.condition;
+    if (!Array.isArray(condition)) { condition = [condition]; }
+
+    // Convert to CodeableConceptView...
+    const conditionCodes: CodeableConcept[] = condition.map((condition: FamilyMemberHistory_Condition) => { return condition.code; });    
+    elConditions = conditionCodes.map((conditionCode: CodeableConcept) => {
+      return <CodeableConceptView codeableConcept={conditionCode} />
+    });
+  }
+  
+  return (
+    <div>
+        <div><strong>FamilyMemberHistoryView</strong></div>
+        {elRelationship} {elSex}
+        <br />
+
+        Conditions: {elConditions}
+    </div>
+  );  
+};
